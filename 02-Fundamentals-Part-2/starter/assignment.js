ef18754de539dd71430e8f17fe72403607f80b6e
hibaq obsiye <obsiyehibaq@gmail.com>
2023-01-22 17:00:33 +0000

    using callback function to calculate scores

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -9,3 +9,19 @@ const describePopulation = (country, population)=> {
 console.log(describePopulation('china', '1441'))
 console.log(describePopulation('UK', '203'))
 console.log(describePopulation('Los Angeles', '303'))
+
+const calcAverage = (scores) => {
+    let sum = 0
+    for(i = 0; i < scores.length; i++){
+      sum += scores[i]   
+    }
+  return sum / scores.length
+  }
+
+const checkWinner = (name1, name2, team1, team2) => {
+    let team1Score = calcAverage(team1)
+    let team2Score = calcAverage(team2)
+
+   return  team1Score > team2Score ? `${name1} is the winner` : `${name2} is the winner`
+}
+console.log(checkWinner('dolphin','Koala',[44,23, 71], [65, 54, 49]))
